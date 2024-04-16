157b790b9d3bdb1f279ece9fb09b9e6f5d61a3f2
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-04-16 15:53:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('http').createServer((req, res) => {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write('Nice To Fuck You');
     res.end();
 }).listen(process.env.PORT || 3000)
