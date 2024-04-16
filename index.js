99d1c187a8c8368d4f56e04552654d0c3e18a222
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-04-16 15:52:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var http = require('http');
-http.createServer(function (req, res) {
+require('http').createServer((req, res) => {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!');
     res.end();
-}).listen(process.env.PORT || 3000);
+}).listen(process.env.PORT || 3000)
