19ef30478494b10e9f7190221bb35302b2d62eef
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-04-16 15:55:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+
+fs.writeFileSync("test.txt", "Hello World " + Math.random(), "utf-8");
+
 require('http').createServer((req, res) => {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Nice To Fuck You');
